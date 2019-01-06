cb24387a306777b0ff7cc2b2630e477ce2b5acbb
Greg Harper <2421527+gdharper@users.noreply.github.com>
2019-01-05 20:57:49 -0800

    Changes architecture to a class-based structure, which contains the program state (#10)

--- FeatExplorer.py
+++ FeatExplorer.py
@@ -1,24 +1,69 @@
 import csv
-import subprocess
 import platform
+import subprocess
+import sys
 
-##########################################################################################
+########################################################################################
 # Global Constants and Context
-##########################################################################################
-inFile = "Feats.csv"
-titleString = "Pathfinder Feat Explorer! Updated Jan 3, 2019\n"
-         
-filtKeys = set(["name", "types", "description", "benefits", "source", "prerequisites",
-                "prerequisite_feats", "prerequisite_skills", "prerequisite_race"])
+########################################################################################
+titleString = "Pathfinder Feat Explorer - Updated Jan 3, 2019\n"
 
-featList = []
-featDict = {}
-filt = {}
-filtList = []
+class ProgramState:
+    filtKeys = set(["name","types","description","benefits","source","prerequisites",
+                    "prerequisite_feats","prerequisite_skills","prerequisite_race"])
 
-#########################################################################################
+    def __init__(self, featList, initialState):
+        self.current_state = initialState
+        self.filt = {}
+        for k in ProgramState.filtKeys: self.filt[k] = {}
+        self.featList = featList
+        self.filteredSelection = []
+        self.applyFilter()
+ 
+    def clearFilterElement(self, key):
+        self.filt[key].clear()
+
+    def clearFilter(self):
+        for key in self.filt.keys():
+            self.clearFilterElement(key)
+
+    def printFilterState(self):
+        print("Current filter state:")
+        if not any(self.filt.values()):
+            print("No filter\n")
+        else:
+            filtStr = ""
+            for k in filter(lambda k: self.filt[k] != {}, self.filt.keys()):
+                inc = [key for key in self.filt[k].keys() if self.filt[k][key]]
+                exc = [key for key in self.filt[k].keys() if not self.filt[k][key]]
+                filtStr += "\t" + k + ": \n"
+                if inc: filtStr += "\t\tIncludes: " + ", ".join(inc) + "\n"
+                if exc: filtStr += "\t\tExcludes: " + ", ".join(exc) + "\n"
+            print(filtStr)
+        print("Feats in current selection: "+str(len(self.filteredSelection)))
+
+    def containsFilter(self, k):
+        return self.filt[k] != {}
+
+    def requireValueIn(self, value, element):
+        self.filt[element][value] = True
+
+    def filterValueFrom(self, value, element):
+        self.filt[element][value] = False
+
+    def applyFilter(self):
+        def do_filter(filt, feat):
+            for k in filt.keys():
+                for key in filt[k].keys():
+                    if (key in feat[k].lower()) != filt[k][key]: return False
+            return True
+
+        self.filteredSelection = [f for f in self.featList if do_filter(self.filt, f)]
+
+
+########################################################################################
 # UI "Prettiness"
-#########################################################################################
+########################################################################################
 def featToString_Brief(feat):
     return feat["name"] + " - " + feat["description"]
 
@@ -32,85 +77,62 @@ def featToString(feat):
 def featToString_Full(feat):
     return featToString(feat)
 
-def filterToString(filtDict):
-    filtStr = ""
-    for k in [k for k in filtKeys if filt[k]]:
-        inc = [key for key in filt[k].keys() if filt[k][key]]
-        exc = [key for key in filt[k].keys() if not filt[k][key]]
-        filtStr += "\t" + k + ": \n"
-        if inc:
-            filtStr += "\t\tIncludes: " + ", ".join(inc) + "\n"
-        if exc:
-            filtStr += "\t\tExcludes: " + ", ".join(exc) + "\n"
-
-    return filtStr
-
-def print_filter_state():
-    fstr = filterToString(filt)
-    if not fstr:
-        fstr = "No Filter\n"
-    else:
-        fstr = "\n" + fstr
-    print("Current Filter State: " + fstr)
-
 def clearConsole():
-    _ = subprocess.run("cls" if platform.system() == "Windows" else "clear", shell = True)
+    _=subprocess.run("cls" if platform.system() == "Windows" else "clear", shell = True)
 
-#########################################################################################
+########################################################################################
 # State Actions
-#########################################################################################
-def view_feats():
-    print("\n".join(map(featToString_Brief, filtList)))
-    _ =input("\nPress [Enter] to return to initial program menu: ")
-    return "base_menu"
+########################################################################################
+def view_feats(state):
+    print("\n".join(map(featToString_Brief, state.filteredSelection)))
+    print() 
+    resp = input("View a feat in detail [yes/no]? ")
+    if "yes".find(resp) == 0: _=print_feat(state)
+    _=input("\nPress [Enter] to return to filtering menu: ")
+    return "edit_filter"
 
-def view_tree():
+def view_tree(state):
     print("view_tree")
     return "base_menu"
 
-def tweak_filter_element(key):
+def determine_prereq_type():
+    print("select prerequisite type to filter:")
+    options = { "1": ("General prerequisites", "prerequisites"),
+                "2": ("Prerequisite feats", "prerequisite_feats"),
+                "3": ("Prerequisite skills", "prerequisite_skills"),
+                "4": ("Prerequisite races", "prerequisite_race")}
+
+    opts = [k + " : " + v[0] for k,v in options.items()]
+    print("\t " + "\n\t ".join(opts))
+
+    response = input("Selection: ")
+    while response not in options.keys(): response = input("Selection: ")
+    return options[response][1]
+
+def tweak_filter_element(state, k):
     clearConsole()
-    print_filter_state()
-    print("Modifying filter based on feat " + key)
-
-    if key == "prerequisites":
-        print("select prerequisite type to filter:")
-        options = { "1": ("General prerequisites", "prerequisites"),
-                    "2": ("Prerequisite feats", "prerequisite_feats"),
-                    "3": ("Prerequisite skills", "prerequisite_skills"),
-                    "4": ("Prerequisite races", "prerequisite_race"),
-                    "q": ("Return to previous menu", "")}
-
-        opts = [k + " : " + v[0] for k,v in options.items()]
-        print("\t " + "\n\t ".join(opts))
-
-        response = ""
-        while response not in options.keys(): response = input("Selection: ")
-        if response == "q": return
-        else: key = options[response][1]
-
-    while filt[key]:
-        response = input("Would you like to clear filtering based on feat " + key + " [yes/no] ? ").lower()
-        if "yes".find(response) == 0:
-            filt[key].clear()
-            print("Removed filtering.")
-        elif "no".find(response) == 0: break
+    state.printFilterState()
+    print("Modifying filter based on feat " + k)
+    
+    if k == "prerequisites": k = determine_prereq_type()
+    if state.containsFilter(k):
+        response = input("Clear existing filtering based on feat " + k + " [yes/no] ? ").lower()
+        if "yes".find(response) == 0: state.clearFilterElement(k)
 
     response = input("Term to add to filter: ").lower()
     while True:
-        resp = input("[Include/Exclude] " + response +" from feat " + key + ": ").lower()
+        resp = input("[Include/Exclude] "+response+" from feat "+k+": ").lower()
         if "include".find(resp) == 0:
-            filt[key][response] = True
+            state.requireValueIn(response, k)
             break
         elif "exclude".find(resp) == 0:
-            filt[key][response] = False 
+            state.filterValueFrom(response, k)
             break
 
-    print("Filtering selection to feats where feat " + key + " " + resp + "s " + response)
-    print_filter_state()    
+    state.printFilterState()    
 
-def modify_filter():
-    print_filter_state()
+def modify_filter(state):
+    state.printFilterState()
     print("Select filter criteria to modify")
     options = {"0" : "Clear Filter",
                "1" : "name",
@@ -125,42 +147,40 @@ def modify_filter():
         if k in ["0", "q"]: print("\t" + k + " : " + v)
         else: print("\t" + k + " : Edit filtering based on: " + v)
 
-    key = input("Enter selection: ")
-    while key not in options.keys(): key = input("Invalid selection.\nEnter selection: ")
+    k = input("Enter selection: ")
+    while k not in options.keys(): k = input("Invalid selection.\nEnter selection: ")
 
-    if key == "0": map(lambda k: filt[k].clear(), filt.keys())
-    elif key == "q": return "edit_filter"
-    else: tweak_filter_element(options[key])   
+    if k == "0": state.clearFilter() 
+    elif k == "q": return "edit_filter"
+    else: tweak_filter_element(state, options[k])   
 
     while True:
-        cont = input("Would you like to continue editing the filter? [yes/no]").lower()
+        cont = input("Continue editing the filter? [yes/no]? ").lower()
         if "yes".find(cont) == 0: return "modify_filter"
         elif "no".find(cont) == 0: return "edit_filter"
 
-def do_filter(feat):
-    for k in [s for s in filt.keys() if filt[s]]:
-        for key in filt[k].keys():
-            if not ((key in feat[k].lower()) == filt[k][key]):
-                return False
-    return True
-
-def apply_filter():
+def apply_filter(state):
     print("Filtering feats")
-    filtList.clear()
-    filtList.extend([f for f in featList if do_filter(f)])
+    state.applyFilter()
     return "edit_filter"
 
-def print_feat():
-    featName = input("Enter the name of the feat you wish to view: ").lower()
-    if featName not in featDict: print("Invalid feat name")
-    else: print(featToString_Full(featDict[featName]))
+def print_feat(state):
+    featDict = {}
+    for feat in state.featList:
+        featDict[feat["name"].lower()] = feat
+    
+    while True: 
+        featName = input("Enter the name of the feat you wish to view: ").lower()
+        print()
+        if featName not in featDict: print("Invalid feat name")
+        else: print(featToString_Full(featDict[featName]))
+        print()
+        while True:
+            retry = input("View another feat? [yes/no]? ").lower()
+            if "no".find(retry) == 0: return "base_menu"
+            elif "yes".find(retry) == 0: break
 
-    while True:
-        retry = input("Would you like to view another feat? [yes/no]").lower()
-        if "yes".find(retry) == 0: return "print_feat"
-        elif "no".find(retry) == 0: return "base_menu"
-
-def exit_program():
+def exit_program(_):
     clearConsole()
     quit()
 
@@ -176,23 +196,24 @@ def simple_state(options):
     while k not in options.keys(): k = input("Invalid selection.\nPlease enter a selection: ")
     return options[k][0]
 
-def edit_filter():
+def edit_filter(state):
+    state.printFilterState()
     options = {"m" : ("modify_filter", "Change elements of current filter"),
                "a" : ("apply_filter", "Apply current filter to feat database"),
+               "v" : ("view_feats", "View filtered selection or output to file"),
                "b" : ("base_menu", "Return to initial program menu")}
     return simple_state(options)
 
-def base_menu():
-    options = { "f" : ("edit_filter", "Update or apply current filter"),
-                "v" : ("view_feats", "View filtered feat selection or output to file"),
-                "t" : ("view_tree", "View filtered feat selection as dependency tree"),
+def base_menu(_):
+    options = { "f" : ("edit_filter", "Filter feat database and view filtered selection"),
                 "p" : ("print_feat", "View a specific feat in the console"),
+                "t" : ("view_tree", "View full feat tree"),
                 "x" : ("exit_program", "Exit program")}
     return simple_state(options)
 
-###############################################################################################################
+########################################################################################
 # State Table
-###############################################################################################################
+########################################################################################
 states = {"base_menu" : base_menu,
           "edit_filter" : edit_filter,
           "view_feats" : view_feats,
@@ -202,26 +223,24 @@ states = {"base_menu" : base_menu,
           "print_feat" : print_feat,
           "exit_program" : exit_program}
 
-###############################################################################################################
+########################################################################################
 # Main Program Functionality
-###############################################################################################################
-def executeState(state):
-    clearConsole()
-    print(titleString)
-    print_filter_state()
-    print("Number of feats in filtered selection: " + str(len(filtList)) + "\n")
-    return states[state]()
-
+########################################################################################
 if __name__ == "__main__":
-    with open(inFile, "r") as csvFile:
+    if len(sys.argv) > 1: iFile = sys.argv[1]
+    else:
+        print(titleString)
+        iFile = input("Path to feat csv file: ")
+    
+    featList = []
+    with open(iFile, "r") as csvFile:
         reader = csv.DictReader(csvFile)
         for row in reader: 
             featList.append(row)
-            featDict[row["name"].lower()] = row
 
-    for k in filtKeys: filt[k] = {}
-    apply_filter()
+    state = ProgramState(featList, "base_menu")
+    while True:
+        clearConsole()
+        print(titleString)
+        state.current_state = states[state.current_state](state)
 
-    current_state = "base_menu"
-    while True: current_state = executeState(current_state)
-        
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Pathfinder_Feat_Explorer
-Some simple python scripts for interacting with the overwhelming database of character feats in the Pathfinder RPG
+Some simple python scripts for interacting with the overwhelming database of character 
+feats in the Pathfinder RPG
 
 ## FeatExplorer.py
 The core script, a console app that can sort through and display feats 
-read from the hardcoded "Feats.csv" file, found i nthe same directory as the script.
+read from a file passed as an argument to the script.
 
 ## ParseFeatCsv.py
 Reads from the "FeatsBase.csv" file, which must be in the same directory as the script.
